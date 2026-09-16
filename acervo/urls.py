eabf06fe38932a0fa2423fa130fb7b886b7145e4
alvesssjean <jeanpaulo370@gmail.com>
2026-09-16 20:21:18 -0300

    feat: urls adicionadas

--- acervo/urls.py
+++ acervo/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path(
+        '', views.inicio,
+        'livros/',
+        views.lista_livros_fisicos,
+        views.lista_livros_digitais,
+        name='lista',
+        ),
 ]
--- acervo/views.py
+++ acervo/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import LivroDigital, LivroFisico
 
 # Create your views here.
 def inicio(request):
     return HttpResponse(
         'Olá, acervo!'
+    )
+    
+def lista_livros_digitais(request):
+    livros = LivroDigital.objects.all()
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros}
+    )
+    
+def lista_livros_fisicos(request):
+    livros = LivroFisico.objects.all()
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros}
     )
